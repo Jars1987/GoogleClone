1984c569d97bcf49d841a5bfed631eebe3b26d7a
José Santos <josesantos@MacBook-Pro-de-Jose.local>
2021-11-02 21:22:06 +0000

    Home and Search Page client side built

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  reactStrictMode: true,
+  images: {
+    domains: ['www.google.co.uk'],
+  },
+};
+
+/*
+module.exports = {
+  env: {
+    API_KEY: 'your API KEY ',
+    CONTEXT_KEY: 'your CONTEXT KEY '
+  },
 }
+
+ search.js 
+:
+const API_KEY  = process.env.API_KEY
+const CONTEXT_KEY = process.env.CONTEXT_KEY
+
+*/
